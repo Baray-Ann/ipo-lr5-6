84c1be9c92fd8dba289e29c26791e69f93cc622a
Baray Ann <annbaray66@gmail.com>
2024-10-20 16:35:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-text=open('text.txt', 'r', encoding="utf-8")
-str1=text.read().split(" ")
-words=open('output.txt', 'w', encoding="utf-8")
-str2=list()
-for word in str1:
-    if word not in str2:
-        words.write(word)
-    str2.append(word)
+text=open('text.txt', 'r', encoding="utf-8") #Открытие файла text.txt для чтения с помощью режима 'r', кодировки utf-8 и присваивание его переменной text
+str1=text.read().split(" ") #Создание списка, содержащего слова текста из файла
+words=open('output.txt', 'w', encoding="utf-8") #Открытие файла output.txt для записи с помощью режима 'w', кодировки utf-8 и присваивание его переменной words
+str2=list() #Создание пустого списка
+for word in str1: #Цикл for для перебора элементов(word) в списке str1
+    if word not in str2: #Цикл if для проверки отсутствия элемента(word) в списке str2
+        words.write(word) #Добавлениие уникального элемента в файл output.txt
+    str2.append(word) #Добавление элемента(word) в список str2
