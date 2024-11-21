5df21c0315303b31f1dbd307bb646609bfdaff7b
Baray Ann <annbaray66@gmail.com>
2024-11-21 10:52:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-text=open('text.txt', 'r', encoding="utf-8") #Открытие файла text.txt для чтения с помощью режима 'r', кодировки utf-8 и присваивание его переменной text
-str1=text.read().split(" ") #Создание списка, содержащего слова текста из файла
-words=open('output.txt', 'w', encoding="utf-8") #Открытие файла output.txt для записи с помощью режима 'w', кодировки utf-8 и присваивание его переменной words
-str2=list() #Создание пустого списка
+text = open('text.txt', 'r', encoding="utf-8") #Открытие файла text.txt для чтения с помощью режима 'r', кодировки utf-8 и присваивание его переменной text
+str1 = text.read().split(" ") #Создание списка, содержащего слова текста из файла
+words = open('output.txt', 'w', encoding="utf-8") #Открытие файла output.txt для записи с помощью режима 'w', кодировки utf-8 и присваивание его переменной words
+str2 = list() #Создание пустого списка
 for word in str1: #Цикл for для перебора элементов(word) в списке str1
     if word not in str2: #Цикл if для проверки отсутствия элемента(word) в списке str2
         words.write(word) #Добавлениие уникального элемента в файл output.txt
